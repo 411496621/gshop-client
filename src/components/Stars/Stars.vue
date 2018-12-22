cfb9ed99f21cd21457e84fcc6f1456f9ada4c055
411496621 <411496621@qq.com>
2018-12-22 20:27:35 +0800

    完成stars组件显示

--- src/components/Stars/Stars.vue
+++ src/components/Stars/Stars.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="star star-24">
-    <span class="star-item on"></span>
-    <span class="star-item on"></span>
-    <span class="star-item on"></span>
-    <span class="star-item half"></span>
-    <span class="star-item off"></span>
+  <div class="star" :class="`star-${size}`">
+    <span class="star-item" :class="star" v-for="(star,index) in stars" :key="index"></span>
   </div>
 </template>
 
@@ -13,24 +9,74 @@
     props:{
       size:Number,
       rating:Number
+    },
+    computed:{
+      stars(){
+        let starClasses = []
+        const onCount = Math.floor(this.rating)
+        for (var i = 0; i < onCount; i++) {
+             starClasses.push("on")
+        }
+        if(Math.round(this.rating-onCount)){
+           starClasses.push("half")
+        }
+        while (starClasses.length<5){
+           starClasses.push("off")
+        }
+        return starClasses
+      }
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
   @import "../../common/stylus/mixins.styl"
-  .star-24
+  .star //2x图 3x图
+    float left
+    font-size 0
     .star-item
-      width 10px
-      height 10px
-      margin-right 3px
-      background-size 10px 10px
-      &:last-child
-        margin-right 0
-      &.on
-        bg-image('./images/star24_on')
-      &.half
-        bg-image('./images/star24_half')
-      &.off
-        bg-image('./images/star24_off')
+      display inline-block
+      background-repeat no-repeat
+    &.star-48
+      .star-item
+        width 20px
+        height 20px
+        margin-right 22px
+        background-size 20px 20px
+        &:last-child
+          margin-right: 0
+        &.on
+          bg-image('./images/star48_on')
+        &.half
+          bg-image('./images/star48_half')
+        &.off
+          bg-image('./images/star48_off')
+    &.star-36
+      .star-item
+        width 15px
+        height 15px
+        margin-right 6px
+        background-size 15px 15px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('./images/star36_on')
+        &.half
+          bg-image('./images/star36_half')
+        &.off
+          bg-image('./images/star36_off')
+    &.star-24
+      .star-item
+        width 10px
+        height 10px
+        margin-right 3px
+        background-size 10px 10px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('./images/star24_on')
+        &.half
+          bg-image('./images/star24_half')
+        &.off
+          bg-image('./images/star24_off')
 </style>
