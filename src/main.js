3aa063d7636572d3dc050041429b0b49a6c1b087
411496621 <411496621@qq.com>
2018-12-22 16:39:06 +0800

    完成Login组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue"
 import App from "./App"
 import router from './router'
-
+import NavHeader from "./components/NavHeader/NavHeader"
+Vue.component('NavHeader',NavHeader)
 new Vue({
   el:'#app',
   render:h=>h(App),
